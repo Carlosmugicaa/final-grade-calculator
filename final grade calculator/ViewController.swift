f35ef5251fd2bf2c671fd9fa5ee705cdcc941b97
Carlos Mugica <cmugica4@cloud.com>
2022-03-04 13:31:45 -0700

    MVP

--- final grade calculator/ViewController.swift
+++ final grade calculator/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var input1: UITextField!
+    
+    @IBOutlet weak var input2: UITextField!
+    
+    @IBOutlet weak var input3: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func button1(_ sender: Any) {
+        
+        var currentgrade = input1.text ?? ""
+        var desiredgrade = input2.text ?? ""
+        var finalgrade = input3.text ?? ""
+        
+        var c = Double(currentgrade) ?? 0.0
+        var d = Double(desiredgrade) ?? 1.1
+        var f = Double(finalgrade) ?? 2.2
+        
+    var decimalweight = f/100
+        
+        var difference = 1 - decimalweight
+        var valueofassignments = difference * c
+        var difference2 = d - valueofassignments
+        var nearfinal = difference2 / f
+        
+        var finalballs = nearfinal * 100
+        
+        
+    label1.text = "you need to score " + "\(finalballs)" + "% on the final exam"
+        
+        
+        
+        
+        
+    }
+    
+    @IBOutlet weak var label1: UILabel!
+  
+    
 
 }
 
